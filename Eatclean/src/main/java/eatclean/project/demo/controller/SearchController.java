9440fa43bb22a9415b4a51a67e47aed693223dda
pham_thi_phuong <21012514@st.phenikaa-uni.edu.vn>
2023-10-29 21:24:01 +0700

    cap nhat

--- Eatclean/src/main/java/eatclean/project/demo/controller/SearchController.java
+++ Eatclean/src/main/java/eatclean/project/demo/controller/SearchController.java
@@ -1,12 +1,27 @@
 package eatclean.project.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import eatclean.project.demo.enity.Products;
+import eatclean.project.demo.service.SearchProService;
 
 @Controller
 public class SearchController {
+
+    @Autowired
+    private SearchProService searchProService;
+
     @GetMapping("/search")
-    public String search(){
+    public String search(@RequestParam(name = "keyword", required = false) String keyword, Model model){
+        List<Products> products = searchProService.search(keyword);
+        model.addAttribute("products", products);
+        model.addAttribute("keyword", keyword!=null ? keyword : "");
         return "search";
     }
 }
---
+++ Eatclean/src/main/java/eatclean/project/demo/service/SearchProService.java
@@ -0,0 +1,52 @@
+package eatclean.project.demo.service;
+
+import java.util.List;
+
+import javax.ws.rs.Produces;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import eatclean.project.demo.enity.Products;
+import eatclean.project.demo.repository.ProductsRepository;
+
+@Service
+public class SearchProService {
+    @Autowired
+    private ProductsRepository productsRepository;
+    public static boolean isNotBlank(String str) {
+        return str != null && str.trim().length() > 0;
+    }
+
+    public List<Products> search(String keyword){
+        if(keyword != null && !keyword.isBlank()){
+            List<Products> products = productsRepository.findByNameContaining(keyword);
+            for(Products product : products){
+                String imageUrl = product.getImage();
+                if(isNotBlank(imageUrl)){
+                    product.setImage(processImageUrl(imageUrl));
+                }else{
+                    imageUrl="https://znews-photo.zingcdn.me/w660/Uploaded/natmts/2023_02_03/z4080571344877_dcc05eb033d9e910039ad77df6eb1b05.jpg";
+                }
+                product.setImage(imageUrl);
+            }
+            return products;
+        }else{
+            List<Products> products = productsRepository.findAll();
+            for(Products product : products){
+                String imageUrl = product.getImage();
+                if(isNotBlank(imageUrl)){
+                    product.setImage(processImageUrl(imageUrl));
+                }else{
+                    imageUrl="https://znews-photo.zingcdn.me/w660/Uploaded/natmts/2023_02_03/z4080571344877_dcc05eb033d9e910039ad77df6eb1b05.jpg";
+                }
+                product.setImage(imageUrl);
+            }
+            return products;
+        }
+
+    }
+    private String processImageUrl(String imageUrl) {
+        return null;
+    }
+}
